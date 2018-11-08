87acf25a8c01a5009048c6fcbda1dfcc36afd319
SiwonHwang <kimshluv@naver.com>
2018-11-08 16:16:48 +0900

    04

--- main.c
+++ main.c
@@ -3,24 +3,31 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+void swap (int *a, int *b)
+{
+	int temp;
 	
-	int grade[5] = {10, 20, 30, 40, 50};
-	int average=0;
-	int *ptr;
-	
-	int i;
-	
-	//pointer setting
-	ptr = grade;
-	
-	//average calculation
-	for (i=0;i<5;i++)
-	{
-		average += *(ptr+i); //ptr[i]
-	}
-	
-	printf("average : %i\n", average/5);
+	temp = *a;
+	*a = *b;
+	*b = temp;
 		
-	return 0;
+	return;
 }
+
+
+int main(void){
+	
+	int x=48;
+	int y=85;
+	
+	printf("x = %i, ", x);
+	printf("y = %i\n", y);
+	
+	swap(&x, &y);
+	
+	printf("x = %i, ", x);
+	printf("y = %i\n", y);
+	
+	return 0;
+} 
+
